1be2aadc9473ff4ea60aefa6771ea00f1d9ed386
Parzi <86732697+ParziDev@users.noreply.github.com>
2021-12-04 13:08:07 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name:"yardım",
+  code:`
+  $author[Selam $username;$authorAvatar]
+  $addField[$getServerVar[prefix]raid-engel aç/kapat/sayı;Raid engel sistemini açıp kapatır]
+  $addField[$getServerVar[prefix]link-engel aç/kapat;Link engel sistemini açıp kapatır]
+  $addField[$getServerVar[prefix]spam-engel aç/kapat;Spam engel sistemini açıp kapatır]
+  $addField[$getServerVar[prefix]küfür-engel aç/kapat;Küfür engel sistemini açıp kapatır]
+  $color[$getServerVar[hex]]
+  $thumbnail[$authorAvatar]
+  `
+  }
